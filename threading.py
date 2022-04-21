73fef15c474a3e9fce6d6819c47a0a41eae074b0
UTP <utp@utp.local>
2022-04-21 10:51:20 -0500

    threading sin funcionar, visto en clase

--- threading.py
+++ threading.py
@@ -3,6 +3,8 @@ def worker(num):
     print("worker: %s" % num)
 threads = []
 for i in range(5):
+    #iteracines que hace el for
     t = threading.Thread(target=worker, args=(i,))
     threads.append(t)
+    #el punto start inicia el hilo
     t.start()
